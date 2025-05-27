19678065b368ab51901cd0e3773c4ed4a90a2660
Hack6297 <164305482+Hack6297@users.noreply.github.com>
2025-05-26 18:43:22 -0700

    Add files via upload

---
+++ server.py
@@ -0,0 +1,40 @@
+from socketio import AsyncServer
+from socketio.asgi import ASGIApp
+from fastapi import FastAPI
+
+sio = AsyncServer(async_mode='asgi', cors_allowed_origins='*')
+app = FastAPI()
+app.mount("/", ASGIApp(sio))
+
+players = {}
+
+@sio.event
+async def connect(sid, environ):
+    print(f"[+] {sid} connected")
+    players[sid] = {'x': 0, 'y': 0, 'z': 0, 'color': (255, 255, 255), 'nickname': 'Player'}
+    await sio.emit('player_update', players)
+
+@sio.event
+async def disconnect(sid):
+    print(f"[-] {sid} disconnected")
+    players.pop(sid, None)
+    await sio.emit('player_update', players)
+
+@sio.event
+async def player_position(sid, data):
+    players[sid] = {
+        'x': data['x'],
+        'y': data['y'],
+        'z': data['z'],
+        'color': data['color'],
+        'nickname': data.get('nickname', 'Player')
+    }
+    await sio.emit('player_update', players)
+
+@sio.event
+async def block_update(sid, data):
+    await sio.emit('block_update', data, skip_sid=sid)
+
+@sio.event
+async def block_remove(sid, data):
+    await sio.emit('block_remove', data, skip_sid=sid)
